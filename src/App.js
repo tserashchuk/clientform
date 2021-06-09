e422c3397b710f1eea562ac9ac5abfc01222eabe
tserashchuk <tserashchuk.kirill@gmail.com>
2021-06-09 15:39:10 +0300

    деплой меня

--- src/App.js
+++ src/App.js
@@ -13,11 +13,17 @@ function App() {
         <>
             <Router>
                 <Switch>
+                    <Route exact path='/' children={Add}/>
                     <Route exact path="/:id" children={RatingUs}/>
                 </Switch>
             </Router>
         </>
     );
+}
+
+
+const Add = () => {
+    return 'we are deployed'
 }
 
 export default App;
