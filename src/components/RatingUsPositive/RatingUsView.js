b3d7b987c12afaa812455ac992f8033782f38c63
tserashchuk <tserashchuk.kirill@gmail.com>
2021-08-02 17:07:04 +0300

    деплой меня

--- src/components/RatingUsPositive/RatingUsView.js
+++ src/components/RatingUsPositive/RatingUsView.js
@@ -103,7 +103,6 @@ const DetailedForm = () => {
         <>
             <form>
                 <div className="form-row">
-                    <div className="form-row">
                     <div className="form-group col-md-6">
                         <label htmlFor="inputEmail4">Email</label>
                         <input type="email" className="form-control" id="inputEmail4" placeholder="Email"/>
@@ -114,7 +113,6 @@ const DetailedForm = () => {
                             <option selected>Да</option>
                             <option>Нет</option>
                         </select>
-                    </div>
                 </div>
                 </div>
                 <div className="form-group">
